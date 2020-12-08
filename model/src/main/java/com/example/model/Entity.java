51fbf45e88e71aa40c66fbabd4a0442a1890334f
pavel m <paullus325@gmail.com>
2020-12-08 12:21:08 +0300

    generation

--- model/src/main/java/com/example/model/Entity.java
+++ model/src/main/java/com/example/model/Entity.java
@@ -1,10 +1,13 @@
 package com.example.model;
 
 import javax.persistence.Column;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
+@javax.persistence.Entity
 @Table(name = "entity")
 public class Entity {
+    @Id
     @Column(name = "status")
     private Status status;
 }
